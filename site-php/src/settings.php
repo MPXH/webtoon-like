671a8ff929c47cd4cc9db25235f2cf982ea359a6
Rémi Bernard <remib18@outlook.fr>
2022-04-04 13:23:07 +0200

    Update settings.php

--- site-php/src/settings.php
+++ site-php/src/settings.php
@@ -2,22 +2,21 @@
 
 namespace WebtoonLike\Site;
 
-const SETTINGS = [
-    'googleTranslateApi' => '',
-    'preTranslateTo' => [
-        'fr',
-        'en'
-    ],
-    'database' => [
-        'host' => 'localhost',
-        'username' => 'root',       // TODO: Replace root in production
-        'password' => null,
-        'dbName' => 'webtoonLike',
-        'port' => null,
-        'socket' => null
-    ]
-];
-
-function getSettings(): array {
-    return SETTINGS;
+function getSettings(): array{
+    return [
+        'googleTranslateApi' => '',
+        'preTranslateTo' => [
+            'fr',
+            'en'
+        ],
+        'webtoonsImagesBaseFolder' => dirname(__DIR__) . '/assets/webtoons-imgs',
+        'database' => [
+            'host' => 'localhost',
+            'username' => 'root',       // TODO: Replace root in production
+            'password' => null,
+            'dbName' => 'webtoonLike',
+            'port' => null,
+            'socket' => null
+        ]
+    ];
 }
